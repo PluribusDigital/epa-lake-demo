207ce71c5d7147bcc0ffc732c837a1e73e5e3bc1
Chris Cassatt <chris.cassatt@gmail.com>
2015-12-28 22:52:47 -0500

    fix

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -29,14 +29,14 @@ aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$EB_ENV/$DOCKERRUN_FILE.zip
 echo "dockerrun file copied"
 
 echo "creating elasticbeanstalk application version..."
-#aws elasticbeanstalk create-application-version --application-name $APP_NAME \
-#  --version-label $BUILD_TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$EB_ENV/$DOCKERRUN_FILE.zip \
-#  --region us-east-1
+aws elasticbeanstalk create-application-version --application-name $APP_NAME \
+  --version-label $BUILD_TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$EB_ENV/$DOCKERRUN_FILE.zip \
+  --region us-east-1
 echo "elasticbeanstalk application version created"
 
 # Update Elastic Beanstalk environment to new version
 echo "deploying elasticbeanstalk application..."
-#aws elasticbeanstalk update-environment --environment-name $EB_ENV \
-#    --version-label $BUILD_TAG --region us-east-1
+aws elasticbeanstalk update-environment --environment-name $EB_ENV \
+    --version-label $BUILD_TAG --region us-east-1
 echo "elasticbeanstalk application deployed"
 cd ../..
