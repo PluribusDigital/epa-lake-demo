affe7b3bcba3158482e08e86984df05b4a34b9c1
Chris Cassatt <chris.cassatt@gmail.com>
2015-12-28 22:47:49 -0500

    fix

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -19,25 +19,24 @@ sed -e "s/<TAG>/$BUILD_TAG/" \
     -e "s/<DOCKER_PROJECT>/$DOCKER_PROJECT/" \
     -e "s/<POSTGRES_USER>/docker/" \
     -e "s/<POSTGRES_PASSWORD>/docker/" \
-    #-e "s/<NEW_RELIC_KEY>/$NEW_RELIC_KEY/" \
     < $DOCKERRUN_FILE.template > $DOCKERRUN_FILE
 
 # elastic beanstalk requires application source to be zipped
-zip -r $DOCKERRUN_FILE.zip $DOCKERRUN_FILE .ebextensions
+zip -r $DOCKERRUN_FILE.zip $DOCKERRUN_FILE
 
 echo "copying dockerrun file to s3 bucket..."
-aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$EB_ENV/$DOCKERRUN_FILE.zip
+#aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$EB_ENV/$DOCKERRUN_FILE.zip
 echo "dockerrun file copied"
 
 echo "creating elasticbeanstalk application version..."
-aws elasticbeanstalk create-application-version --application-name $APP_NAME \
-  --version-label $BUILD_TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$EB_ENV/$DOCKERRUN_FILE.zip \
-  --region us-east-1
+#aws elasticbeanstalk create-application-version --application-name $APP_NAME \
+#  --version-label $BUILD_TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$EB_ENV/$DOCKERRUN_FILE.zip \
+#  --region us-east-1
 echo "elasticbeanstalk application version created"
 
 # Update Elastic Beanstalk environment to new version
 echo "deploying elasticbeanstalk application..."
-aws elasticbeanstalk update-environment --environment-name $EB_ENV \
-    --version-label $BUILD_TAG --region us-east-1
+#aws elasticbeanstalk update-environment --environment-name $EB_ENV \
+#    --version-label $BUILD_TAG --region us-east-1
 echo "elasticbeanstalk application deployed"
 cd ../..
