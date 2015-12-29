2d87c7e6b100b3d7c757987eeb54cba76261172b
Ben Morris <git@benmorris.net>
2015-12-29 11:24:31 -0500

    clean up graph

--- app/graph/query_root.rb
+++ app/graph/query_root.rb
@@ -29,18 +29,4 @@ QueryRoot = GraphQL::ObjectType.define do
     }
   end
 
-  field :lake_file_data do 
-    type -> {types[LakeFileDataType]}
-    description "data from a given file for a lake"
-    argument :site_id, !types.ID, "Site ID of lake"
-    argument :file, !types.String, "File Name to match"
-    argument :visit_no, !types.String, "Visit Number to match"
-    resolve -> (object, arguments, context) {
-      LakeData.data_for_file(
-        arguments[:site_id],
-        arguments[:file],
-        arguments[:visit_no])
-    }
-  end
-
 end
--- app/graph/types/lake_file_data_type.rb
+++
@@ -1,6 +0,0 @@
-LakeFileDataType = GraphQL::ObjectType.define do
-  name "LakeFileDataField"
-  description "Values for lake data"
-  field :field, !types.String, 'File name of original EPA file'
-  field :value, !types.String, 'value in field'
-end
